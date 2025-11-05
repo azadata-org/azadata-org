0abf82d381eb5a5ecf7df2dec84444db918e2523
Danial Khoshkhou <31406378+danial-k@users.noreply.github.com>
2025-11-05 07:47:54 +0000

    Add non-UK company datasets with API fetching
    Adds basic company data files for non-UK datasets.

--- frontend/web/public/src/components/countryCompanies.tsx
+++ frontend/web/public/src/components/countryCompanies.tsx
@@ -1,13 +1,38 @@
+import { useAppSelector } from "../app/hooks"
 import { CountryCode } from "../data/countryCodes"
+import { useGetCompaniesQuery } from "../features/api/apiSlice"
+import { selectCountryCode } from "../features/country/countrySlice"
 
 type CountryCompaniesProps = {
   countryCode: CountryCode
 }
 
 export default function CountryCompanies(props: CountryCompaniesProps) {
-  return (
-    <>
-      No company results found for country: {props.countryCode.englishShortName}
-    </>
-  )
+  const selectedCountryCode = useAppSelector(selectCountryCode)
+  if (selectedCountryCode) {
+    const { data, isError, isLoading, isSuccess } = useGetCompaniesQuery(
+      selectedCountryCode.alpha2Code,
+    )
+    if (isLoading) {
+      return <>Loading...</>
+    }
+    if (isError) {
+      return <>Error loading records</>
+    }
+    if (isSuccess && data) {
+      return (
+        <>
+          {data.map(company => {
+            return <p>{company.name}</p>
+          })}
+        </>
+      )
+    }
+    return (
+      <>
+        No company results found for country:{" "}
+        {props.countryCode.englishShortName}
+      </>
+    )
+  }
 }
---
+++ frontend/web/public/src/features/api/apiSlice.ts
@@ -0,0 +1,30 @@
+// Need to use the React-specific entry point to import `createApi`
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+type Company = {
+  payload_uid: string
+  number: string
+  name: string
+  status: string
+  category: string
+  country_of_origin: string
+}
+
+// Define a service using a base URL and expected endpoints
+export const apiSlice = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: "/" }),
+  reducerPath: "api",
+  // Tag types are used for caching and invalidation.
+  tagTypes: ["Companies"],
+  endpoints: build => ({
+    getCompanies: build.query<Company[], string>({
+      query: alpha2CountryCode =>
+        `uk/companies-house/basic-company-data/${alpha2CountryCode}.json`,
+      // `providesTags` determines which 'tag' is attached to the
+      // cached data returned by the query.
+      providesTags: (_result, _error, uid) => [{ type: "Companies", uid }],
+    }),
+  }),
+})
+
+export const { useGetCompaniesQuery } = apiSlice
